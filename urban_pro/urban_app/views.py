bc9c59c8e11689d3edba4762238ed5c6b8da44a5
kavya <setavakavya3@gmail.com>
2023-12-09 15:48:20 +0530

    interlock setting

--- urban_pro/urban_app/views.py
+++ urban_pro/urban_app/views.py
@@ -354,39 +354,44 @@ def about_process(request):
             else:
 
                 prev_process_update_entry = process_update.objects.filter(manufacture_id=manufacture_id,process_id__lt=process_id).order_by("process_id").first()
-                print('prev_process_update_entry',prev_process_update_entry)
-                prev_status = prev_process_update_entry.status
-                print('prev_status',prev_status)
 
-                if prev_status != "Completed":
+                print('prev_process_update_entry',prev_process_update_entry)
+                if prev_process_update_entry:
+                    prev_status = prev_process_update_entry.status
+                    print('prev_status',prev_status)
+
+                    if prev_status != "Completed":
+                        result = "start"
+                        lock_status = "locked"
+                        status="Not Started"
+                    else:
+                        # result = "start"
+                        lock_status = "unlocked"
+                        # status = "Not Started"
+
+                        current_process_update_entry= process_update.objects.filter(manufacture_id=manufacture_id,process_id=process_id)
+                        if current_process_update_entry:
+                            if current_process_update_entry and current_process_update_entry.status=="On Going":
+                                result = "stop"
+                                # lock_status = "unlocked"
+                                status="On Going"
+                            elif current_process_update_entry.status=="Completed":
+                                result = "start"
+                                # lock_status = "unlocked"
+                                status = "Completed"
+                            else:
+                                result = "start"
+                                # lock_status = "unlocked"
+                                status = "Not Started"
+                        else:
+                            result = "start"
+                            lock_status = "unlocked"
+                            status = "Not Started"
+
+                else:
                     result = "start"
                     lock_status = "locked"
-                    status="Not Started"
-                else:
-                    # result = "start"
-                    lock_status = "unlocked"
-                    # status = "Not Started"
-
-                    current_process_update_entry= process_update.objects.filter(manufacture_id=manufacture_id,process_id=process_id)
-
-                    if current_process_update_entry and current_process_update_entry.status=="On Going":
-                        result = "stop"
-                        # lock_status = "unlocked"
-                        status="On Going"
-                    elif current_process_update_entry.status=="Completed":
-                        result = "start"
-                        # lock_status = "unlocked"
-                        status = "Completed"
-                    else:
-                        result = "start"
-                        # lock_status = "unlocked"
-                        status = "Not Started"
-
-
-
-
-
-
+                    status = "Not Started"
 
             data = {
                 "status": result,
