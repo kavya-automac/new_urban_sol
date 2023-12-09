c751fa23acaaf0f30fd9ef5c6919e29eb5bb7b6d
kavya <setavakavya3@gmail.com>
2023-12-09 18:21:42 +0530

    router settings

--- urban_pro/routers/db_routers.py
+++ urban_pro/routers/db_routers.py
@@ -7,23 +7,23 @@ class AuthRouter:
         return None
 
 
-def db_for_write(self, model, **hints):
-    if model._meta.app_label in self.route_app_labels:
-        return 'mysql_views_db'
-    return None
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label in self.route_app_labels:
+            return 'mysql_views_db'
+        return None
 
 
-# def allow_relation(self, obj1, obj2, **hints):
-#     if (
-#             obj1._meta.app_label in self.route_app_labels or
-#             obj2._meta.app_label in self.route_app_labels
-#     ):
-#         return True
-#     return None
+    # def allow_relation(self, obj1, obj2, **hints):
+    #     if (
+    #             obj1._meta.app_label in self.route_app_labels or
+    #             obj2._meta.app_label in self.route_app_labels
+    #     ):
+    #         return True
+    #     return None
 
 
-def allow_migrate(self, db, app_label, model_name=None, **hints):
-    if app_label in self.route_app_labels:
-        return db == 'mysql_views_db'
-    return None
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.route_app_labels:
+            return db == 'mysql_views_db'
+        return None
 
