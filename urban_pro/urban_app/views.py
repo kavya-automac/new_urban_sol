e976e2c2a9f707d119715dce17e713a46f1c67e6
kavya <setavakavya3@gmail.com>
2024-03-20 14:58:44 +0530

    changed timer (timefield to durationfield)

--- urban_pro/urban_app/views.py
+++ urban_pro/urban_app/views.py
@@ -293,7 +293,6 @@ def start_stop_process(request):
             # manufacture_instance = Manufacture.objects.get(manufacture_No=f_manufacture_id)
             process_instance = Process_Details.objects.get(pk=f_process_id)#Process_Details to Groups
             # print('/////', manufacture_instance, process_instance)
-            timer=timedelta(hours=20,minutes=30,seconds=10)
             start_new_record = process_update(manufacture_id=f_manufacture_id, process_id=process_instance,
                                               start_date=f_start_date, end_date="1111-11-11", timer=timer,
                                               start_time=f_start_time,issues="", status=f_process_status)
